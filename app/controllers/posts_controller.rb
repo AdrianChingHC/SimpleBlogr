62cc70689d0470c781af017cf33e52a3e751199f
AdrianChingHC <achc92@gmail.com>
2015-02-11 15:39:23 +0800

    Add post function

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,8 @@
 class PostsController < ApplicationController
+	before_action :set_post, only:[:show]
 
 	def index
-
+		@posts = Post.all.order('created_at DESC')
 	end
 
 	def new
@@ -9,6 +10,22 @@ class PostsController < ApplicationController
 	end
 
 	def create
+		@post = Post.new(post_params)
+
+		@post.save
+		redirect_to @post
+	end
+	def show
 
 	end
+
+	private
+
+		def set_post
+			@post = Post.find(params[:id])
+		end
+
+		def post_params
+			params.require(:post).permit(:title, :body)
+		end
 end
